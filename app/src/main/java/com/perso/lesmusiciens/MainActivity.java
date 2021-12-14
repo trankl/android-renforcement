31d6fffa6f5e47bd818d454786d0563eb1a0cc01
DORANCO <doranco@doranco.fr>
2021-12-14 12:07:49 +0100

    Création instance MusicienModel
    Ajout infos dans Toast lors du Clic sur le bouton Ajouter

--- app/src/main/java/com/perso/lesmusiciens/MainActivity.java
+++ app/src/main/java/com/perso/lesmusiciens/MainActivity.java
@@ -46,7 +46,14 @@ public class MainActivity extends AppCompatActivity {
         m_btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "CLICK SUR BOUTON AJOUTER", Toast.LENGTH_LONG).show();
+                String l_str_nomMusicien = m_editText_nomMusicien.getText().toString();
+                int l_int_nombreEtoiles = Integer.parseInt(m_editText_nombreEtoiles.getText().toString());
+                boolean l_bool_musicienActif = m_switch_musicienActif.isActivated();
+
+                // On va créer une nouvelle instance de MusicienModel
+                MusicienModel l_obj_musicienModel = new MusicienModel(-1, l_str_nomMusicien, l_int_nombreEtoiles, l_bool_musicienActif);
+
+                Toast.makeText(MainActivity.this, "CLICK SUR BOUTON AJOUTER NOM ["+l_str_nomMusicien+"] / ETOILES ["+l_int_nombreEtoiles+"] / ACTIF ["+l_bool_musicienActif+"]", Toast.LENGTH_LONG).show();
             }
         });
 
